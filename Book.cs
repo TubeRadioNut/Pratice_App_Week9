f1c3b0882eca92b657cd0aa2538bdb82412dfb68
rt65s <rt65s@SCHOOL_LAPTOP>
2024-05-29 17:48:20 -0700

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratice_App_Week9
+{
+    public class Book
+    {
+        //fields
+        string _title;
+        string _author;
+        //constructor
+        public Book(string title, string author)
+        {
+            _title = title;
+            _author = author;
+        }
+        //properties
+        public string Title { get => _title; set => _title = value; }
+        public string Author { get => _author; set => _author = value; }
+    }
+}
